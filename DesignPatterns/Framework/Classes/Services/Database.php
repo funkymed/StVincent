0df534ae08d7874e93731e32dc90b54c8bbc20d3
Cyril Pereira <cyril.pereira@gmail.com>
2021-11-30 14:51:23 +0100

    Update DesignPatterns/Framework/Classes/Services/Database.php
    Co-authored-by: Monteiro Hugo <61043665+divinoPV@users.noreply.github.com>

--- DesignPatterns/Framework/Classes/Services/Database.php
+++ DesignPatterns/Framework/Classes/Services/Database.php
@@ -4,13 +4,13 @@ namespace Classes\Services;
 use Classes\SingletonAbstract;
 use PDO;
 
-class Database extends SingletonAbstract {
+class Database extends SingletonAbstract
+{
     /**
      * @return PDO
      */
     public function getConnection()
     {
-        $pdo = new PDO('mysql:host=192.168.99.100:3306;dbname=framework', 'root', 'root');
-        return $pdo;
+        return new PDO('mysql:host=192.168.99.100:3306;dbname=framework', 'root', 'root');
     }
 }
